488a28c31580a463bb2c85dce877abb44856e964
Yeison Manuel Mestra Mestra <ymmestram@ufpso.edu.co>
2025-05-19 12:06:42 -0500

    cambio de url

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
